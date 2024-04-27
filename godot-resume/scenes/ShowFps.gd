d6f4e912a4b285e23cee77d5cc8f3d5758031a7b
lluancarlo <luanveloz@gmail.com>
2024-04-27 16:44:24 +0200

    feat: show fps

---
+++ godot-resume/scenes/ShowFps.gd
@@ -0,0 +1,16 @@
+extends HBoxContainer
+class_name ShowFps
+
+@export var _label : Label
+var nodeNamesToGuess = ["Value", "value"]
+
+func _ready():
+	if (_label == null):
+		for child in get_children():
+			if (child.name in nodeNamesToGuess):
+				_label = child
+				break
+	assert(_label != null, "Must define label to show value")
+
+func _process(delta):
+	_label.text = str(Engine.get_frames_per_second())
