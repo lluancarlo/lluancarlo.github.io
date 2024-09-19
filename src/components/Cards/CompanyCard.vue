3c1c8849be5a7522c873e581a0b6ae53544e1864
lluancarlo <luanveloz@gmail.com>
2024-09-19 17:28:59 +0200

    feat: calculate automatically period on CompanyCard

--- src/components/Cards/CompanyCard.vue
+++ src/components/Cards/CompanyCard.vue
@@ -6,10 +6,31 @@ const props = defineProps({
 function formattedDate(date: Date): string {
   return date.toLocaleString('en-us', { month: 'short', year: 'numeric' })
 }
-function dateIsToday(date: Date): Boolean {
+function IsDateToday(date: Date): Boolean {
   let today = new Date()
   return today.getDate() === date.getDate() && today.getMonth() === date.getMonth()
 }
+function calculatePeriodBetweenDates(startDate: Date, endDate: Date): String {
+  let yearsDifference = endDate.getFullYear() - startDate.getFullYear();
+  let monthsDifference = endDate.getMonth() - startDate.getMonth();
+
+  if (monthsDifference < 0) {
+      yearsDifference--;
+      monthsDifference += 12;
+  }
+  
+  let result = ""
+  if (yearsDifference > 0)
+    result = `${yearsDifference} year${yearsDifference > 1 ? 's' : ''}`
+  if (monthsDifference > 0){
+    if (result.length > 0)
+      result += ' and '
+    result += `${monthsDifference} month${monthsDifference > 1 ? 's' : ''}`
+  }
+  
+  return result;
+}
+
 </script>
 
 <template>
@@ -18,24 +39,29 @@ function dateIsToday(date: Date): Boolean {
       <img :src="props.company.logo" :alt="props.company.name" />
       <h3>{{ props.company.name }}</h3>
     </a>
-    <div class="company-position">
+    <div class="card-line">
       <h4>at</h4>
       <p>{{ props.company.location }}</p>
     </div>
-    <div class="company-position">
+    <div class="card-line">
       <h4>as</h4>
       <p>{{ props.company.position }}</p>
     </div>
-    <div class="company-position">
+    <div class="card-line">
       <h4>from</h4>
       <p>
         {{ formattedDate(props.company.startDate) }} to
         {{
-          dateIsToday(props.company.endDate) ? 'Present' : formattedDate(props.company.startDate)
+          IsDateToday(props.company.endDate) ? 'Present' : formattedDate(props.company.endDate)
         }}
       </p>
     </div>
-    <div class="company-position">
+    <div class="card-line">
+      <p>
+        {{ calculatePeriodBetweenDates(props.company.startDate, props.company.endDate) }}
+      </p>
+    </div>
+    <div class="card-line">
       <p>{{ props.company.description }}</p>
     </div>
   </div>
@@ -58,16 +84,16 @@ function dateIsToday(date: Date): Boolean {
   background-color: #f4d99c90;
   transform: scale(1.05);
 }
-.company-position {
+.card-line {
   display: inline-flex;
   gap: 0.5em;
   align-items: center;
 }
-.company-position h4 {
+.card-line h4 {
   color: #000;
   margin: 0;
 }
-.company-position p {
+.card-line p {
   color: #000;
   margin: 0;
 }
